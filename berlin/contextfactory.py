a6289056e247c2058faf51c3ff0d2c58d5532486
Justin Salisbury[D <justin@salisbury.de>
2016-01-17 21:32:59 +0100

    attribution

--- berlin/contextfactory.py
+++ berlin/contextfactory.py
@@ -1,3 +1,5 @@
+##
+# FROM http://stackoverflow.com/questions/31027587/crawling-ssl-site-with-scrapy
 # file: myproject/contextfactory.py
 from OpenSSL import SSL
 from scrapy.core.downloader.contextfactory import ScrapyClientContextFactory
