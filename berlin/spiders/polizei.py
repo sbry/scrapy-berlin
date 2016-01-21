b7e860b06e855c864334c6dfc30f613b4091ec1e
Justin Salisbury <justin@salisbury.de>
2016-01-21 23:14:43 +0100

    use the rss because it has better pubDate

--- berlin/spiders/polizei.py
+++ berlin/spiders/polizei.py
@@ -10,15 +10,28 @@ from datetime import datetime
 class PolizeiSpider(scrapy.Spider):
     name = "polizei"
     allowed_domains = ["www.berlin.de"]
+    ##
+    # we parse the rss-feed for the date-created timestamp
     start_urls = (
-        'https://www.berlin.de/polizei/polizeimeldungen/',
-    )
-    def parse(self, content):
-        for href in content.css('li.row-fluid > div:nth-child(2) > a:nth-child(1)::attr("href")'):
-            full_url = content.urljoin(href.extract())
-            yield scrapy.Request(full_url, callback=self.parse_item_page)
+        'https://www.berlin.de/polizei/polizeimeldungen/index.php/rss',
+    )       
+    def parse(self, response):
+        for rss_item in response.xpath('//item'):
+            full_url = rss_item.xpath('link/text()').extract()[0]
+            request = scrapy.Request(full_url, callback=self.parse_item_page)
+            ##
+            # send the timestamp in "meta" (recommended procedure)
+            pub_date = rss_item.xpath('pubDate/text()').extract()[0]
+            pub_date = "-".join(pub_date.split()[1:5])
+            request.meta['time'] = datetime.strptime(pub_date , "%d-%b-%Y-%H:%M:%S")
+            ##
+            # does not work (ValueError: 'z' is a bad directive in format '%a, %d %b %Y %H:%M:%S %z')
+            # request.meta['time'] = datetime.strptime(pub_date , "%a, %d %b %Y %H:%M:%S %z")
+            yield request
         pass
     def parse_item_page(self, response):
+        ##
+        # retrieve what we sent
         selector = response.css('div.article')
         item_loader = BerlinItemLoader(selector = selector)
         ##
@@ -36,12 +49,14 @@ class PolizeiSpider(scrapy.Spider):
         parts['source_id'] = match.group(1)
         ##
         # parse time time
-        raw_time = selector.css('div.polizeimeldung:nth-child(1)::text')
-        parts['time'] = datetime.strptime(raw_time.extract()[0], 'Polizeimeldung vom %d.%m.%Y')
+        if response.meta['time']:
+            parts['time'] = response.meta['time']
+        else:
+            raw_time = selector.css('div.polizeimeldung:nth-child(1)::text')
+            parts['time'] = datetime.strptime(raw_time.extract()[0], 'Polizeimeldung vom %d.%m.%Y')
         ##
         # and add the parts
         item_loader.add_value(None, parts)
         return item_loader.load_item()
     pass
-    
 
